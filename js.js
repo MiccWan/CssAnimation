3d8f40fba50b7a3b2e567c6712eb4c1ce3e072f1
michaelwan <michaelwan0906@hotmail.com>
2016-11-10 17:07:26 +0800

    first

---
+++ js.js
@@ -0,0 +1,493 @@
+//01
+function bounceA(){
+	document.getElementById("bounce").className += " animated bounce";
+}
+
+function bounceB(){
+	document.getElementById("bounce").className = "block";
+}
+
+function bounceInA(){
+	document.getElementById("bounceIn").className += " animated bounceIn";
+}
+
+function bounceInB(){
+	document.getElementById("bounceIn").className = "block";
+}
+function bounceInDownA(){
+	document.getElementById("bounceInDown").className += " animated bounceInDown";
+}
+
+function bounceInDownB(){
+	document.getElementById("bounceInDown").className = "block";
+}
+function bounceInLeftA(){
+	document.getElementById("bounceInLeft").className += " animated bounceInLeft";
+}
+
+function bounceInLeftB(){
+	document.getElementById("bounceInLeft").className = "block";
+}
+function bounceInRightA(){
+	document.getElementById("bounceInRight").className += " animated bounceInRight";
+}
+
+function bounceInRightB(){
+	document.getElementById("bounceInRight").className = "block";
+}
+function bounceInUpA(){
+	document.getElementById("bounceInUp").className += " animated bounceInUp";
+}
+
+function bounceInUpB(){
+	document.getElementById("bounceInUp").className = "block";
+}
+function bounceOutA(){
+	document.getElementById("bounceOut").className += " animated bounceOut";
+}
+
+function bounceOutB(){
+	document.getElementById("bounceOut").className = "block";
+}
+function bounceOutDownA(){
+	document.getElementById("bounceOutDown").className += " animated bounceOutDown";
+}
+
+function bounceOutDownB(){
+	document.getElementById("bounceOutDown").className = "block";
+}
+function bounceOutLeftA(){
+	document.getElementById("bounceOutLeft").className += " animated bounceOutLeft";
+}
+
+function bounceOutLeftB(){
+	document.getElementById("bounceOutLeft").className = "block";
+}
+function bounceOutRightA(){
+	document.getElementById("bounceOutRight").className += " animated bounceOutRight";
+}
+
+function bounceOutRightB(){
+	document.getElementById("bounceOutRight").className = "block";
+}
+function bounceOutUpA(){
+	document.getElementById("bounceOutUp").className += " animated bounceOutUp";
+}
+
+function bounceOutUpB(){
+	document.getElementById("bounceOutUp").className = "block";
+}
+
+//02
+
+function fadeInA(){
+	document.getElementById("fadeIn").className += " animated fadeIn";
+}
+
+function fadeInB(){
+	document.getElementById("fadeIn").className = "block";
+}
+function fadeInDownA(){
+	document.getElementById("fadeInDown").className += " animated fadeInDown";
+}
+
+function fadeInDownB(){
+	document.getElementById("fadeInDown").className = "block";
+}
+function fadeInLeftA(){
+	document.getElementById("fadeInLeft").className += " animated fadeInLeft";
+}
+
+function fadeInLeftB(){
+	document.getElementById("fadeInLeft").className = "block";
+}
+function fadeInRightA(){
+	document.getElementById("fadeInRight").className += " animated fadeInRight";
+}
+
+function fadeInRightB(){
+	document.getElementById("fadeInRight").className = "block";
+}
+function fadeInUpA(){
+	document.getElementById("fadeInUp").className += " animated fadeInUp";
+}
+
+function fadeInUpB(){
+	document.getElementById("fadeInUp").className = "block";
+}
+function fadeOutA(){
+	document.getElementById("fadeOut").className += " animated fadeOut";
+}
+
+function fadeOutB(){
+	document.getElementById("fadeOut").className = "block";
+}
+function fadeOutDownA(){
+	document.getElementById("fadeOutDown").className += " animated fadeOutDown";
+}
+function fadeOutDownB(){
+	document.getElementById("fadeOutDown").className = "block";
+}
+function fadeOutLeftA(){
+	document.getElementById("fadeOutLeft").className += " animated fadeOutLeft";
+}
+
+function fadeOutLeftB(){
+	document.getElementById("fadeOutLeft").className = "block";
+}
+function fadeOutRightA(){
+	document.getElementById("fadeOutRight").className += " animated fadeOutRight";
+}
+
+function fadeOutRightB(){
+	document.getElementById("fadeOutRight").className = "block";
+}
+function fadeOutUpA(){
+	document.getElementById("fadeOutUp").className += " animated fadeOutUp";
+}
+function fadeOutUpB(){
+	document.getElementById("fadeOutUp").className = "block";
+}
+//03
+
+function flipA(){
+	document.getElementById("flip").className += " animated flip";
+}
+
+function flipB(){
+	document.getElementById("flip").className = "block";
+}
+function flipInXA(){
+	document.getElementById("flipInX").className += " animated flipInX";
+}
+
+function flipInXB(){
+	document.getElementById("flipInX").className = "block";
+}
+function flipInYA(){
+	document.getElementById("flipInY").className += " animated flipInY";
+}
+
+function flipInYB(){
+	document.getElementById("flipInY").className = "block";
+}
+function flipOutXA(){
+	document.getElementById("flipOutX").className += " animated flipOutX";
+}
+
+function flipOutXB(){
+	document.getElementById("flipOutX").className = "block";
+}
+function flipOutYA(){
+	document.getElementById("flipOutY").className += " animated flipOutY";
+}
+
+function flipOutYB(){
+	document.getElementById("flipOutY").className = "block";
+}
+
+//04
+function lightSpeedInA(){
+	document.getElementById("lightSpeedIn").className += " animated lightSpeedIn";
+}
+
+function lightSpeedInB(){
+	document.getElementById("lightSpeedIn").className = "block";
+}
+function lightSpeedOutA(){
+	document.getElementById("lightSpeedOut").className += " animated lightSpeedOut";
+}
+
+function lightSpeedOutB(){
+	document.getElementById("lightSpeedOut").className = "block";
+}
+
+//05
+function rotateInA(){
+	document.getElementById("rotateIn").className += " animated rotateIn";
+}
+
+function rotateInB(){
+	document.getElementById("rotateIn").className = "block";
+}
+function rotateInDownLeftA(){
+	document.getElementById("rotateInDownLeft").className += " animated rotateInDownLeft";
+}
+
+function rotateInDownLeftB(){
+	document.getElementById("rotateInDownLeft").className = "block";
+}
+function rotateInDownRightA(){
+	document.getElementById("rotateInDownRight").className += " animated rotateInDownRight";
+}
+
+function rotateInDownRightB(){
+	document.getElementById("rotateInDownRight").className = "block";
+}
+function rotateInUpLeftA(){
+	document.getElementById("rotateInUpLeft").className += " animated rotateInUpLeft";
+}
+
+function rotateInUpLeftB(){
+	document.getElementById("rotateInUpLeft").className = "block";
+}
+function rotateInUpRightA(){
+	document.getElementById("rotateInUpRight").className += " animated rotateInUpRight";
+}
+
+function rotateInUpRightB(){
+	document.getElementById("rotateInUpRight").className = "block";
+}
+function rotateOutA(){
+	document.getElementById("rotateOut").className += " animated rotateOut";
+}
+
+function rotateOutB(){
+	document.getElementById("rotateOut").className = "block";
+}
+function rotateOutDownLeftA(){
+	document.getElementById("rotateOutDownLeft").className += " animated rotateOutDownLeft";
+}
+
+function rotateOutDownLeftB(){
+	document.getElementById("rotateOutDownLeft").className = "block";
+}
+function rotateOutDownRightA(){
+	document.getElementById("rotateOutDownRight").className += " animated rotateOutDownRight";
+}
+
+function rotateOutDownRightB(){
+	document.getElementById("rotateOutDownRight").className = "block";
+}
+function rotateOutUpLeftA(){
+	document.getElementById("rotateOutUpLeft").className += " animated rotateOutUpLeft";
+}
+
+function rotateOutUpLeftB(){
+	document.getElementById("rotateOutUpLeft").className = "block";
+}
+function rotateOutUpRightA(){
+	document.getElementById("rotateOutUpRight").className += " animated rotateOutUpRight";
+}
+
+function rotateOutUpRightB(){
+	document.getElementById("rotateOutUpRight").className = "block";
+}
+
+//06
+function slideInUpA(){
+	document.getElementById("slideInUp").className += " animated slideInUp";
+}
+
+function slideInUpB(){
+	document.getElementById("slideInUp").className = "block";
+}
+function slideInDownA(){
+	document.getElementById("slideInDown").className += " animated slideInDown";
+}
+
+function slideInDownB(){
+	document.getElementById("slideInDown").className = "block";
+}
+function slideInLeftA(){
+	document.getElementById("slideInLeft").className += " animated slideInLeft";
+}
+
+function slideInLeftB(){
+	document.getElementById("slideInLeft").className = "block";
+}
+function slideInRightA(){
+	document.getElementById("slideInRight").className += " animated slideInRight";
+}
+
+function slideInRightB(){
+	document.getElementById("slideInRight").className = "block";
+}
+
+function slideOutUpA(){
+	document.getElementById("slideOutUp").className += " animated slideOutUp";
+}
+
+function slideOutUpB(){
+	document.getElementById("slideOutUp").className = "block";
+}
+function slideOutDownA(){
+	document.getElementById("slideOutDown").className += " animated slideOutDown";
+}
+
+function slideOutDownB(){
+	document.getElementById("slideOutDown").className = "block";
+}
+function slideOutLeftA(){
+	document.getElementById("slideOutLeft").className += " animated slideOutLeft";
+}
+
+function slideOutLeftB(){
+	document.getElementById("slideOutLeft").className = "block";
+}
+function slideOutRightA(){
+	document.getElementById("slideOutRight").className += " animated slideOutRight";
+}
+
+function slideOutRightB(){
+	document.getElementById("slideOutRight").className = "block";
+}
+
+
+//07
+function zoomInA(){
+	document.getElementById("zoomIn").className += " animated zoomIn";
+}
+
+function zoomInB(){
+	document.getElementById("zoomIn").className = "block";
+}
+function zoomInDownA(){
+	document.getElementById("zoomInDown").className += " animated zoomInDown";
+}
+
+function zoomInDownB(){
+	document.getElementById("zoomInDown").className = "block";
+}
+function zoomInLeftA(){
+	document.getElementById("zoomInLeft").className += " animated zoomInLeft";
+}
+
+function zoomInLeftB(){
+	document.getElementById("zoomInLeft").className = "block";
+}
+function zoomInRightA(){
+	document.getElementById("zoomInRight").className += " animated zoomInRight";
+}
+
+function zoomInRightB(){
+	document.getElementById("zoomInRight").className = "block";
+}
+function zoomInUpA(){
+	document.getElementById("zoomInUp").className += " animated zoomInUp";
+}
+
+function zoomInUpB(){
+	document.getElementById("zoomInUp").className = "block";
+}
+function zoomOutA(){
+	document.getElementById("zoomOut").className += " animated zoomOut";
+}
+
+function zoomOutB(){
+	document.getElementById("zoomOut").className = "block";
+}
+function zoomOutDownA(){
+	document.getElementById("zoomOutDown").className += " animated zoomOutDown";
+}
+
+function zoomOutDownB(){
+	document.getElementById("zoomOutDown").className = "block";
+}
+function zoomOutLeftA(){
+	document.getElementById("zoomOutLeft").className += " animated zoomOutLeft";
+}
+
+function zoomOutLeftB(){
+	document.getElementById("zoomOutLeft").className = "block";
+}
+function zoomOutRightA(){
+	document.getElementById("zoomOutRight").className += " animated zoomOutRight";
+}
+
+function zoomOutRightB(){
+	document.getElementById("zoomOutRight").className = "block";
+}
+function zoomOutUpA(){
+	document.getElementById("zoomOutUp").className += " animated zoomOutUp";
+}
+
+function zoomOutUpB(){
+	document.getElementById("zoomOutUp").className = "block";
+}
+
+
+//08
+function flashA(){
+	document.getElementById("flash").className += " animated flash";
+}
+
+function flashB(){
+	document.getElementById("flash").className = "block";
+}
+function pulseA(){
+	document.getElementById("pulse").className += " animated pulse";
+}
+
+function pulseB(){
+	document.getElementById("pulse").className = "block";
+}
+function rubberBandA(){
+	document.getElementById("rubberBand").className += " animated rubberBand";
+}
+
+function rubberBandB(){
+	document.getElementById("rubberBand").className = "block";
+}
+function shakeA(){
+	document.getElementById("shake").className += " animated shake";
+}
+
+function shakeB(){
+	document.getElementById("shake").className = "block";
+}
+function swingA(){
+	document.getElementById("swing").className += " animated swing";
+}
+
+function swingB(){
+	document.getElementById("swing").className = "block";
+}
+function tadaA(){
+	document.getElementById("tada").className += " animated tada";
+}
+
+function tadaB(){
+	document.getElementById("tada").className = "block";
+}
+function wobbleA(){
+	document.getElementById("wobble").className += " animated wobble";
+}
+
+function wobbleB(){
+	document.getElementById("wobble").className = "block";
+}
+function jelloA(){
+	document.getElementById("jello").className += " animated jello";
+}
+
+function jelloB(){
+	document.getElementById("jello").className = "block";
+}
+function hingeA(){
+	document.getElementById("hinge").className += " animated hinge";
+}
+
+function hingeB(){
+	document.getElementById("hinge").className = "block";
+}
+function rollInA(){
+	document.getElementById("rollIn").className += " animated rollIn";
+}
+
+function rollInB(){
+	document.getElementById("rollIn").className = "block";
+}
+function rollOutA(){
+	document.getElementById("rollOut").className += " animated rollOut";
+}
+
+function rollOutB(){
+	document.getElementById("rollOut").className = "block";
+}
+
+
+
+
+
+
